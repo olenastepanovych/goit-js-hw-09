df1e231b956e04df1adf78bba291315b437ac67a
Lena <stlena113@gmail.com>
2025-02-01 19:53:36 +0200

    fifth commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,15 +3,13 @@ const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 const STORAGE_KEY = 'feedback-form-state';
 
-// Об'єкт для збереження даних форми
 let formData = { email: '', message: '' };
 
-// Функція для оновлення локального сховища
+
 const updateStorage = () => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
-// Завантажуємо дані з localStorage, якщо вони є
 const savedData = localStorage.getItem(STORAGE_KEY);
 if (savedData) {
     formData = JSON.parse(savedData);
@@ -19,17 +17,17 @@ if (savedData) {
     messageInput.value = formData.message || '';
 }
 
-// Обробник події input (оновлення локального сховища)
+
 form.addEventListener('input', (event) => {
     formData[event.target.name] = event.target.value.trim();
     updateStorage();
 });
 
-// Обробник події submit
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    // Перевірка, чи всі поля заповнені
+
     const email = emailInput.value.trim();
     const message = messageInput.value.trim();
 
@@ -40,7 +38,7 @@ form.addEventListener('submit', (event) => {
 
     console.log({ email, message });
 
-    // Очищення localStorage та форми
+
     localStorage.removeItem(STORAGE_KEY);
     formData = { email: '', message: '' };
     form.reset();
